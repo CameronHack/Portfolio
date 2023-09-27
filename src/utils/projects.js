dc6c4315ee22da91019254e6ebd1e76be835ee98
Cam <camhack67@gmail.com>
2023-09-27 11:27:06 -0400

    project images

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -6,7 +6,7 @@ const projects = [
         id: 1,
         title: "ARV",
         description: "",
-        image: {ARVDesktop},
+        image: `${ARVDesktop}`,
         url: "https://cameronhack.github.io/ARV/",
         github: "https://github.com/CameronHack/ARV",
         tags: ["Bootstrap", "Bing maps", "AirLabs Flight Tracker"]
@@ -15,7 +15,7 @@ const projects = [
         id: 2,
         title: "VoteWave",
         description: "",
-        image: {VoteWaveDesktop},
+        image: `${VoteWaveDesktop}`,
         url: "https://damp-beach-24784-675a670756cd.herokuapp.com/",
         github: "https://github.com/brianmyer/votewave",
         tags: ["Handlebars", "Node", "Express", "MySQL", "Tailwind", "Chart.js"]
